9e9b38131903547a92e773ab275f5a4eed7a77b3
Breno Carvalho <brenovulcun174@gmail.com>
2025-02-27 01:53:08 -0300

    add logic of message processing and twilio integration

--- src/server.js
+++ src/server.js
@@ -15,6 +15,32 @@ app.get("/", (req, res) => {
   res.send("Chatbot online e funcionando!");
 });
 
+app.post("/webhook", (req, res) => {
+  const incommingMessage = req.body.Body;
+  console.log(incommingMessage);
+  const from = req.body.From;
+  const To = process.env.TWILIO_PHONE_NUMBER;
+
+  console.log("Mensagem recebida:", incommingMessage);
+  console.log("De:", from);
+
+  const responseMessage = `Você disse: ${incommingMessage}`;
+
+  client.messages
+    .create({
+      body: responseMessage,
+      from: To,
+      to: from,
+    })
+    .then(() => {
+      res.status(200).send("Mensagem enviada!");
+    })
+    .catch((err) => {
+      console.error("Erro ao enviar mensagem:", err);
+      res.status(500).send("Erro ao enviar mensagem.");
+    });
+});
+
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
